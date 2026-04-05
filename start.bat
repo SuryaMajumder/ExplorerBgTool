a59644f1321cd4176a3685e99776399c7fdef827
SuryaMajumder <111181100+SuryaMajumder@users.noreply.github.com>
2026-04-04 21:50:28 -0400

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,1 +1,2 @@
-powershell -Command "Start-Process python -ArgumentList 'explorer_bg_tool.py' -Verb RunAs"
+powershell -Command "Start-Process cmd -ArgumentList '/k cd /d %~dp0 && pip install pillow --quiet && python explorer_bg_tool.py' -Verb RunAs"
+REM powershell -Command "Start-Process python -ArgumentList 'explorer_bg_tool.py' -Verb RunAs"
